2f0f888afa3252975ff1d3f22c6350adae49fa98
kmuzzi10 <kmuzammil12546@gmail.com>
2023-08-02 03:01:09 +0500

    some changes

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ inquirer
     const url = answers.URL;
     var qr_svg = qr.image(url);
 qr_svg.pipe(fs.createWriteStream('qr_img.png'));
+
+fs.writeFile("url.txt",url,(err)=>{
+  if(err)throw(err);
+  console.log('file has been saved');
+})
     
   })
   .catch((error) => {
